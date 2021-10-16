43bf2e4c6ffdbb96d2df244794f0dd4a640160e0
Mayya Markova <mayya.markova@gmail.com>
2021-10-16 10:41:32 +0300

    Hello-world demo

---
+++ hello-world/main.go
@@ -0,0 +1,12 @@
+package main // Go files always start with package statement
+
+// Import block, it allows importing multiple packages
+import "fmt" // Automatically adds this line, if we forgot it
+
+/*
+The main function, when part of the main package,
+identifies the entry point of an application
+*/
+func main() {
+	fmt.Println("Hello, world!") // go run main.go
+}
