625084709701a69e3311ae2635fea51d4c5c74e5
Mayya Markova <mayya.markova@gmail.com>
2021-10-17 09:17:57 +0300

    Constants demo

---
+++ 05.constants/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const pi = 3.14 // the value should be available on run time => can't be the result of a function
+	fmt.Println(pi) // 3.14
+	// pi = 3.142 => error
+
+	const a = 3          // implicit type constant
+	fmt.Println(a + 3)   // 6
+	fmt.Println(a + 1.2) // 4.2 => no error
+
+	const b int = 1
+	fmt.Println(b + 3) // 4
+	// fmt.Println(b + 1.2) //  error
+}
